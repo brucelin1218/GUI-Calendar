b31f3457608ef2a5e87591e14950b93047c3b424
Bruce Lin <brucelin1218@gmail.com>
2017-11-21 16:02:26 -0800

    Update SimpleCalendar.java

--- SimpleCalendar.java
+++ SimpleCalendar.java
@@ -1,13 +1,5 @@
 import java.io.IOException;
 
-/**
- * CS151
- * Homework Assignment #4: GUI Calendar
- * Instructor: Dr. Kim 
- * @author BruceLin
- * Due Date: 11/20/2017
- */
-
 public class SimpleCalendar {
 	/**
 	 * The driver for the GUI Calendar.
